7c68b4608b625fce6f5e48afcc4d3616617f1aae
Abdullah Nashaat <ab_nashaat@hotmail.com>
2022-12-20 23:43:00 +0200

    Create Log In Form
    Create LogIn view which will launch at the start of the WPF ShellView, add PasswordBoxHelper for dealing easily with Passsword Box, change folder name from view to views for good Caliburn.micro conventions

--- TRMDesktopUI/Views/ShellView.xaml.cs
+++ TRMDesktopUI/Views/ShellView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace TRMDesktopUI.View
+namespace TRMDesktopUI.Views
 {
     /// <summary>
     /// Interaction logic for ShellView.xaml
